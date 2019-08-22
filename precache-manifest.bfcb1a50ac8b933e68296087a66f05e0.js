6f548171b505c1a88f3407b35abc4de66607ca15
Jairo Bilbao <jairoaussie@gmail.com>
2019-08-22 21:15:41 +1000

    Updates

---
+++ precache-manifest.bfcb1a50ac8b933e68296087a66f05e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c359ad5a733d48a0f0d974e8e8cdec",
+    "url": "/photo-gallery/index.html"
+  },
+  {
+    "revision": "8c7f1a321a2c903dd574",
+    "url": "/photo-gallery/static/css/main.662d42a8.chunk.css"
+  },
+  {
+    "revision": "1437a317cff92f31d5df",
+    "url": "/photo-gallery/static/js/2.9d939e36.chunk.js"
+  },
+  {
+    "revision": "8c7f1a321a2c903dd574",
+    "url": "/photo-gallery/static/js/main.a4d0f52a.chunk.js"
+  },
+  {
+    "revision": "ea7b3f6ae33e6a721b19",
+    "url": "/photo-gallery/static/js/runtime~main.bc35ab36.js"
+  }
+]);
